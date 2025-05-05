63b7acbf205e45d471fa4c8d13c939cf33ad8124
douglassqueiroz18 <douglas.queiroz25@outlook.com>
2025-05-05 13:45:46 -0400

    inserindo o frontend corretamente, com standalone false

--- frontend_helpdesk/src/app/app.component.ts
+++ frontend_helpdesk/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{title}}!</h1>
 
     <router-outlet />
   `,
-  styles: [],
+  standalone: false,
+  styles: []
 })
 export class AppComponent {
   title = 'frontend_helpdesk';
