b266a91306c0d8d7a0036d8a7341464beba9d6a6
douglassqueiroz18 <douglas.queiroz25@outlook.com>
2025-05-05 15:31:34 -0400

    Component Home criado

--- frontend_helpdesk/src/app/app.module.ts
+++ frontend_helpdesk/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { NavComponent } from './components/nav/nav.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend_helpdesk/src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styles: ``
+})
+export class HomeComponent {
+
+}
